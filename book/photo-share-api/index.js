8a410e164f86e88f29d8bb948cc83d8771ad1381
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-06-12 23:34:10 +0900

    book/photo-share-api Photo、Query、Mutationを定義

--- book/photo-share-api/index.js
+++ book/photo-share-api/index.js
@@ -1,14 +1,26 @@
+const fs = require('fs')
+const path = require('path')
 const { ApolloServer } = require('apollo-server')
 
-const typeDefs = `
-  type Query {
-    totalPhotos: Int!
-  }
-`
+const typeDefs = fs.readFileSync(path.join(__dirname, 'graphql/schema/schema.graphql'), { encoding: 'utf-8' })
+
+let _id = 0
+const photos = []
 
 const resolvers = {
   Query: {
-    totalPhotos: () => 42,
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      const newPhoto = {
+        id: _id++,
+        ...args,
+      }
+      photos.push(newPhoto)
+      return newPhoto
+    },
   },
 }
 
