29d8244596f11aa67a4fdca0e20da4eb83ef6ea5
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-06-13 08:15:07 +0900

    book/photo-share-api データをJSONファイルに

--- book/photo-share-api/index.js
+++ book/photo-share-api/index.js
@@ -4,40 +4,8 @@ const { ApolloServer } = require('apollo-server')
 
 const typeDefs = fs.readFileSync(path.join(__dirname, 'graphql/schema.graphql'), { encoding: 'utf-8' })
 
-const users = [
-  { githubUser: 'mHattrup', name: 'Mike Hattrup' },
-  { githubUser: 'gPlake', name: 'Glen Plake' },
-  { githubUser: 'sSchmidt', name: 'Scot Schmidt' },
-]
-const photos = [
-  {
-    id: '0',
-    name: 'Sample photo 1',
-    description: 'A sample photo',
-    category: 'SELFIE',
-    githubUser: 'mHattrup',
-  },
-  {
-    id: '1',
-    name: 'Dropping the Heart Chute',
-    description: 'The heart chute is one of my favorite chutes',
-    category: 'ACTION',
-    githubUser: 'gPlake',
-  },
-  {
-    id: '2',
-    name: 'Enjoying the sunshine',
-    category: 'SELFIE',
-    githubUser: 'sSchmidt',
-  },
-  {
-    id: '3',
-    name: 'Gunbarrel 25',
-    description: '25 laps on gunbarrel today',
-    category: 'LANDSCAPE',
-    githubUser: 'sSchmidt',
-  },
-]
+const users = require('./data/users.json')
+const photos = require('./data/photos.json')
 let _id = photos.length
 
 const resolvers = {
@@ -64,6 +32,4 @@ const resolvers = {
   },
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
-
-server.listen().then(({ url }) => console.log(`GraphQL Server running on ${url}`))
+new ApolloServer({ typeDefs, resolvers }).listen().then(({ url }) => console.log(`GraphQL Server running on ${url}`))
