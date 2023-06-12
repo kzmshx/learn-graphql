af3ee48c2c44ae52764c2768d5db3fd374024ab0
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-06-12 23:51:29 +0900

    book/photo-share-api 入力型を追加

--- book/photo-share-api/index.js
+++ book/photo-share-api/index.js
@@ -2,12 +2,15 @@ const fs = require('fs')
 const path = require('path')
 const { ApolloServer } = require('apollo-server')
 
-const typeDefs = fs.readFileSync(path.join(__dirname, 'graphql/schema/schema.graphql'), { encoding: 'utf-8' })
+const typeDefs = fs.readFileSync(path.join(__dirname, 'graphql/schema.graphql'), { encoding: 'utf-8' })
 
 let _id = 0
 const photos = []
 
 const resolvers = {
+  Photo: {
+    url: (parent) => `https://www.example.com/img/${parent.id}.jpg`,
+  },
   Query: {
     totalPhotos: () => photos.length,
     allPhotos: () => photos,
@@ -16,7 +19,7 @@ const resolvers = {
     postPhoto(parent, args) {
       const newPhoto = {
         id: _id++,
-        ...args,
+        ...args.input,
       }
       photos.push(newPhoto)
       return newPhoto
