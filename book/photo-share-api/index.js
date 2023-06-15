1f2538bb65d96080cafa7c3b6e4055c4b1a599c5
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-06-15 21:26:30 +0900

    book/photo-share-api apollo-server-express

--- book/photo-share-api/index.js
+++ book/photo-share-api/index.js
@@ -1,55 +1,23 @@
-const fs = require('fs')
-const path = require('path')
-const { ApolloServer } = require('apollo-server')
+const { readFileSync } = require('fs');
+const path = require('path');
 
-const typeDefs = fs.readFileSync(path.join(__dirname, 'graphql/schema.graphql'), { encoding: 'utf-8' })
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+const expressPlayground = require('graphql-playground-middleware-express').default;
 
-const users = require('./data/users.json')
-const photos = require('./data/photos.json')
-const tags = require('./data/tags.json')
-const { GraphQLScalarType } = require('graphql/type')
-let _id = photos.length
+const resolvers = require('./resolvers');
 
-const resolvers = {
-  DateTime: new GraphQLScalarType({
-    name: 'DateTime',
-    description: 'A valid date time value',
-    parseLiteral: ast => ast.value,
-    parseValue: value => new Date(value),
-    serialize: value => new Date(value).toISOString(),
-  }),
-  User: {
-    postedPhotos: parent => photos.filter(p => p.githubUser === parent.githubUser),
-    inPhotos: parent =>
-      tags
-        .filter(t => t.userID === parent.githubUser)
-        .map(t => t.photoID)
-        .map(photoID => photos.find(p => p.id === photoID)),
-  },
-  Photo: {
-    url: parent => `https://www.example.com/img/${parent.id}.jpg`,
-    postedBy: parent => users.find(u => u.githubUser === parent.githubUser),
-    taggedUsers: parent =>
-      tags
-        .filter(t => t.photoID === parent.id)
-        .map(t => t.userID)
-        .map(userID => users.find(u => u.githubUser === userID)),
-  },
-  Query: {
-    totalPhotos: () => photos.length,
-    allPhotos: () => photos,
-  },
-  Mutation: {
-    postPhoto(parent, args) {
-      const newPhoto = {
-        id: _id++,
-        ...args.input,
-        createdAt: new Date(),
-      }
-      photos.push(newPhoto)
-      return newPhoto
-    },
-  },
-}
+const typeDefs = readFileSync(path.join(__dirname, 'graphql/schema.graphql'), { encoding: 'utf-8' });
 
-new ApolloServer({ typeDefs, resolvers }).listen().then(({ url }) => console.log(`GraphQL Server running on ${url}`))
+(async () => {
+  const app = express();
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+  app.listen({ port: 4000 }, () => console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`));
+})();
---
+++ book/photo-share-api/resolvers.js
@@ -0,0 +1,50 @@
+const { GraphQLScalarType } = require('graphql/type');
+
+const photos = require('./data/photos.json');
+const tags = require('./data/tags.json');
+const users = require('./data/users.json');
+let _id = photos.length;
+
+const resolvers = {
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'A valid date time value',
+    parseLiteral: ast => ast.value,
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+  }),
+  User: {
+    postedPhotos: parent => photos.filter(p => p.githubUser === parent.githubUser),
+    inPhotos: parent =>
+      tags
+        .filter(t => t.userID === parent.githubUser)
+        .map(t => t.photoID)
+        .map(photoID => photos.find(p => p.id === photoID)),
+  },
+  Photo: {
+    url: parent => `https://www.example.com/img/${parent.id}.jpg`,
+    postedBy: parent => users.find(u => u.githubUser === parent.githubUser),
+    taggedUsers: parent =>
+      tags
+        .filter(t => t.photoID === parent.id)
+        .map(t => t.userID)
+        .map(userID => users.find(u => u.githubUser === userID)),
+  },
+  Query: {
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      const newPhoto = {
+        id: _id++,
+        ...args.input,
+        createdAt: new Date(),
+      };
+      photos.push(newPhoto);
+      return newPhoto;
+    },
+  },
+};
+
+module.exports = resolvers;
