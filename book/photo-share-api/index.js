38d6049f2f231a8bc2b0f5c36b2cf5db3cb56733
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-06-15 21:58:03 +0900

    book/photo-share-api mongodbをcontextにセット

--- book/photo-share-api/index.js
+++ book/photo-share-api/index.js
@@ -2,22 +2,35 @@ const { readFileSync } = require('fs');
 const path = require('path');
 
 const { ApolloServer } = require('apollo-server-express');
+const dotenv = require('dotenv');
 const express = require('express');
 const expressPlayground = require('graphql-playground-middleware-express').default;
+const { MongoClient } = require('mongodb');
 
 const resolvers = require('./resolvers');
+
+dotenv.config();
 
 const typeDefs = readFileSync(path.join(__dirname, 'graphql/schema.graphql'), { encoding: 'utf-8' });
 
 (async () => {
   const app = express();
+  const MONGODB_HOST = process.env.MONGODB_HOST;
 
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const client = await MongoClient.connect(MONGODB_HOST, { useNewUrlParser: true });
+  const db = client.db();
+
+  const context = { db };
+
+  const server = new ApolloServer({ typeDefs, resolvers, context });
   await server.start();
+
   server.applyMiddleware({ app });
 
   app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
   app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
 
-  app.listen({ port: 4000 }, () => console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`));
+  app.listen({ port: 4000 }, () => {
+    console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`);
+  });
 })();
