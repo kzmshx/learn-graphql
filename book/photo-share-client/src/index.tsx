1e7e6abc5705ab6b6672aee063be6b9b708f9fc2
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-06-18 00:41:02 +0900

    book/photo-share-client serverからデータ取得してみる

--- book/photo-share-client/src/index.tsx
+++ book/photo-share-client/src/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import ApolloClient, { gql } from 'apollo-boost';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' });
+
+const query = gql`
+  {
+    totalUsers
+    totalPhotos
+  }
+`;
+
+client
+  .query({ query })
+  .then(({ data }) => console.log('data', data))
+  .catch(console.error);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
