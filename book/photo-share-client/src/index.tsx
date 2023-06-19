3b18868eed4539aec58e497b08ce4f0dcb4ced87
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-06-19 23:52:02 +0900

    book/photo-share-client キャッシュの永続化

--- book/photo-share-client/src/index.tsx
+++ book/photo-share-client/src/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { InMemoryCache, NormalizedCacheObject } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { persistCache } from 'apollo-cache-persist';
+import { PersistedData, PersistentStorage } from 'apollo-cache-persist/types';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const cache = new InMemoryCache();
+await persistCache({
+  cache,
+  storage: localStorage as PersistentStorage<PersistedData<NormalizedCacheObject>>,
+});
+if (localStorage['apollo-cache-persist']) {
+  const cacheData = JSON.parse(localStorage['apollo-cache-persist']);
+  cache.restore(cacheData);
+}
+
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
+  cache,
   // Add the authorization header to each request
   request: operation => {
     operation.setContext((context: any) => ({
