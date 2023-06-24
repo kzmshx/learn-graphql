9b40993dfce50b9b6c523a653f6e2db0f81e39df
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-06-24 14:12:40 +0900

    ts-prisma-yoga/messages-client tailwindcss セットアップ

--- ts-prisma-yoga/messages-client/src/App.tsx
+++ ts-prisma-yoga/messages-client/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h2>Hello World!</h2>
+  return (
+    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
+      <h2 className="text-4xl text-yellow-500">Hello, world!</h2>
+    </div>
+  )
 }
 
 export default App
