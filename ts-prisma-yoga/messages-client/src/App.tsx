41acbbb99c924440b897c6ef9e5c8eece3404b24
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-06-26 17:51:06 +0900

    ts-prisma-yoga/messages-client User を表示

--- ts-prisma-yoga/messages-client/src/App.tsx
+++ ts-prisma-yoga/messages-client/src/App.tsx
@@ -1,7 +1,19 @@
+import { User } from './types'
+import UserDisplay from './components/UserDisplay.tsx'
+
 function App() {
+  const users: User[] = [
+    {
+      name: 'Kazumasa Hirata',
+      messages: [{ body: 'Prisma rocks!!' }, { body: 'TypeScript rocks!!' }],
+    },
+  ]
+
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
-      <h2 className="text-4xl text-yellow-500">Hello, world!</h2>
+      {users.map((user, i) => (
+        <UserDisplay user={user} key={i}></UserDisplay>
+      ))}
     </div>
   )
 }
