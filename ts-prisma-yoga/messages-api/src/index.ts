00e38dc50f125d0283183727ea1286c11451169d
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-06-27 19:30:45 +0900

    ts-prisma-yoga/messages-api Setup GraphQL Yoga server

--- ts-prisma-yoga/messages-api/src/index.ts
+++ ts-prisma-yoga/messages-api/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import { createServer } from 'node:http'
+import { createYoga } from '@graphql-yoga/node'
+
+const port = Number(process.env.PORT) || 4000
+
+const yoga = createYoga({})
+const server = createServer(yoga)
+
+server.listen(port, () => {
+  console.info(`Server is running on http://localhost:${port}/graphql`)
+})
